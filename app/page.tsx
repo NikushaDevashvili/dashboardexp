c52cbd7538a2910b0f7a40c00a2a8d3fb9d8fa96
Nick Devashvili <nikadevashvili@gmail.com>
2025-09-05 02:21:20 +0400

    added charts and new card components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Anchor } from "lucide-react";
+import { AppBarChart } from "@/components/AppBarChart";
+import CardList from "@/components/CardList";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-        <div className="bg-primary-foreground p-4 rounded-md">Test1</div>
-        <div className="bg-primary-foreground p-4 rounded-md">Test2</div>
+        <div className="bg-primary-foreground p-4 rounded-md">
+          <AppBarChart />
+        </div>
+        <div className="bg-primary-foreground p-4 rounded-md">
+          <CardList title="Latest Transactions" />
+        </div>
         <div className="bg-primary-foreground p-4 rounded-md">Test3</div>
         <div className="bg-primary-foreground p-4 rounded-md">Test4</div>
-        <div className="bg-primary-foreground p-4 rounded-md">Test5</div>
+        <div className="bg-primary-foreground p-4 rounded-md">
+          <CardList title="Popular Content" />
+        </div>
         <div className="bg-primary-foreground p-4 rounded-md">Test6</div>
       </div>
     </>
